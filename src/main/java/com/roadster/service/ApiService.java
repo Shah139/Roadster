81e1de75ea09f4f84610b83ef3ae3e868b384261
Ahmed Abrar <abrarjubah82@gmail.com>
2025-09-11 11:19:37 +0600

    Shesh

--- src/main/java/com/roadster/service/ApiService.java
+++ src/main/java/com/roadster/service/ApiService.java
@@ -8,7 +8,7 @@ import com.roadster.models.PoliceStation;
 import com.roadster.models.Driver;
 
 public class ApiService {
-    private static final String API_URL = "https://8569284e503d.ngrok-free.app/api/";
+    private static final String API_URL = "https://db974dabe391.ngrok-free.app/api/";
 
     public static List<PoliceStation> fetchPoliceStations() throws Exception {
         String apiUrl = API_URL + "police-stations";
